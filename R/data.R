a19a97883291de9b938113a8917a8f473d4c60dc
Randy Johnson <johnson@hood.edu>
2026-02-11 12:24:50 -0500

    Add MSfrags data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+# data.R
+# Documentation for our data
+
+#' MSfrags
+#' Sample peptide fragments in Spectonaut format
+#' 
+#' @format A character vector containing 4 sample peptide fragments
+#'   with protein modifications in Spectronaut format
+#' @source These fragments were generated from P51681
+#'   (for details, see ...)
+"MSfrags"
