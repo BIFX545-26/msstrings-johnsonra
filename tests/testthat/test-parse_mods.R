9decaa50ded5b277d0f630f6690cf99e7ce504ae
Randy Johnson <johnson@hood.edu>
2026-02-17 20:02:17 -0500

    Add testing

---
+++ tests/testthat/test-parse_mods.R
@@ -0,0 +1,28 @@
+# test the parse_mods function
+
+library(MSstrings)
+
+data("MSfrags")
+
+test_that("parse_mods works", {
+  
+  # test that a peptide with a single mod works
+  tmp <- parse_mods(MSfrags[1])
+  
+  expect_s3_class(tmp, 'protein_mods')
+  expect_equal(tmp$mods[[1]], 'Glycosylation')
+  expect_equal(tmp$mods_at[[1]], 6)
+
+  # test that a peptide with two mods works
+  tmp <- parse_mods(MSfrags[3])
+  
+  expect_equal(tmp$mods[[1]], c("Acetylation", "Deamidation"))
+  expect_equal(tmp$mods_at[[1]], c(6, 31))
+
+  # test that a peptide with no mods works
+  tmp <- parse_mods(MSfrags[4])
+  
+  expect_length(tmp$mods[[1]], 0)
+  expect_length(tmp$mods_at[[1]], 0)
+  
+})
